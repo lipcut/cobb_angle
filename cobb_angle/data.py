1e06d08101e0e9ee3e9492c3ae92bbb05b624d29
UULuceR <hunter70413@gmail.com>
2024-07-27 03:56:15 +0800

    overhaul to solve coupling

--- cobb_angle/data.py
+++ cobb_angle/data.py
@@ -1,64 +1,58 @@
 import os
+from pathlib import Path
+from typing import Callable, List, Optional, Tuple
 
-import cv2
+import numpy as np
+from PIL import Image
 from scipy.io import loadmat
-from torch.utils import data
+from torchvision.datasets.vision import VisionDataset
 
-from . import pre_proc
+from .utils import landmarks_rearrange
 
 
-class BaseDataset(data.Dataset):
-    def __init__(self, data_dir, phase, input_h=1024, input_w=512, down_ratio=4):
-        super(BaseDataset, self).__init__()
-        self.data_dir = data_dir
-        self.phase = phase
-        self.input_h = input_h
-        self.input_w = input_w
-        self.down_ratio = down_ratio
-        self.class_name = ["__background__", "cell"]
-        self.num_classes = 68
-        self.img_dir = os.path.join(data_dir, "data", self.phase)
-        self.img_ids = sorted(os.listdir(self.img_dir))
+class LandmarkDataset(VisionDataset):
+    def __init__(
+        self,
+        root: str | Path,
+        train: bool = True,
+        transforms: Optional[Callable] = None,
+    ):
+        super().__init__(root=root, transforms=transforms)
+        self.phase = "train" if train else "test"
+        self.image_filenames = sorted(os.listdir(self.image_folder))
+        self.data, self.targets = self._load_data()
 
-    def load_annotation(self, index):
-        img_id = self.img_ids[index]
-        annotation_dir = os.path.join(
-            self.data_dir, "labels", self.phase, img_id + ".mat"
-        )
-        pts = loadmat(annotation_dir)["p2"]
-        return pts
+    @property
+    def image_folder(self) -> str:
+        return os.path.join(self.root, "data", self.phase)
 
-    def __getitem__(self, index):
-        img_id = self.img_ids[index]
-        image = cv2.imread(os.path.join(self.img_dir, img_id))
-        if self.phase == "test":
-            images = pre_proc.processing_test(
-                image=image, input_h=self.input_h, input_w=self.input_w
+    @property
+    def landmarks_folder(self) -> str:
+        return os.path.join(self.root, "labels", self.phase)
+
+    def _load_data(self) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+        images = [
+            Image.open(os.path.join(self.image_folder, image_filename))
+            for image_filename in self.image_filenames
+        ]
+
+        landmarks = [
+            landmarks_rearrange(
+                loadmat(os.path.join(self.image_folder, image_filename))["p2"]
             )
-            return {"images": images, "img_id": img_id}
-        else:
-            aug_label = False
-            if self.phase == "train":
-                aug_label = True
-            pts = self.load_annotation(index)  # num_obj x h x w
-            out_image, pts_2 = pre_proc.processing_train(
-                image=image,
-                pts=pts,
-                image_h=self.input_h,
-                image_w=self.input_w,
-                down_ratio=self.down_ratio,
-                aug_label=aug_label,
-                img_id=img_id,
-            )
+            for image_filename in self.image_filenames
+        ]
+        return images, landmarks
 
-            data_dict = pre_proc.generate_ground_truth(
-                image=out_image,
-                pts_2=pts_2,
-                image_h=self.input_h // self.down_ratio,
-                image_w=self.input_w // self.down_ratio,
-                img_id=img_id,
-            )
-            return data_dict
+    def __getitem__(self, index: int) -> Tuple[List[np.ndarray], List[np.ndarray], str]:
+        image = self.data[index]
+        landmarks = self.targets[index]
+        image_filename = self.image_filenames[index]
+
+        if self.transforms is not None:
+            image, landmarks = self.transforms(image, landmarks)
+
+        return image, landmarks, image_filename
 
     def __len__(self):
-        return len(self.img_ids)
+        return len(self.data)
