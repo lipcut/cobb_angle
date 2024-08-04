small fix on scaling

cobb_angle/dsnt.py
@@ -58,8 +58,12 @@ def render_gaussian_2d(
     dist_x: torch.Tensor = (pos_x - mean[..., 0, None, None]) ** 2
     dist_y: torch.Tensor = (pos_y - mean[..., 1, None, None]) ** 2
 
-    kx_s: torch.Tensor = -0.5 * torch.reciprocal(torch.tensor(std) / (width / 2)) ** 2
-    ky_s: torch.Tensor = -0.5 * torch.reciprocal(torch.tensor(std) / (height / 2)) ** 2
+    kx_s: torch.Tensor = (
+        -0.5 * torch.reciprocal(2 * torch.tensor(std) / (width - 1)) ** 2
+    )
+    ky_s: torch.Tensor = (
+        -0.5 * torch.reciprocal(2 * torch.tensor(std) / (height - 1)) ** 2
+    )
 
     gauss: torch.Tensor = torch.exp(dist_x * kx_s) * torch.exp(dist_y * ky_s)
     scaling: torch.Tensor = torch.reciprocal(

cobb_angle/loss.py
@@ -1,10 +1,11 @@
 from typing import Tuple
+import math
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .dsnt import dsnt, render_gaussian_2d
+from .dsnt import dsnt
 
 
 class WingLoss(nn.Module):
@@ -70,8 +71,8 @@ class WingLossWithRegularization(nn.Module):
         _, _, height, width = input[0].shape
 
         target_resized = torch.zeros_like(target)
-        target_resized[..., 0] = 2 * target[..., 0] / width - 1
-        target_resized[..., 1] = 2 * target[..., 1] / height - 1
+        target_resized[..., 0] = 2 * target[..., 0] / (width - 1) - 1
+        target_resized[..., 1] = 2 * target[..., 1] / (height - 1) - 1
 
         stage1, stage2 = input
 